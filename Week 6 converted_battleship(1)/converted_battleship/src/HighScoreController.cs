eb1bb14eb1deeb7e61c59d9d711605b10b678526
TeradonGenesis <poketrainereddie@gmail.com>
2019-04-16 18:27:18 +0800

    LL Look for "Item(...)", remove ".Item" and replace (...) with [...] in HighScore Controller

--- Week 6 converted_battleship(1)/converted_battleship/src/HighScoreController.cs
+++ Week 6 converted_battleship(1)/converted_battleship/src/HighScoreController.cs
@@ -132,7 +132,7 @@ static class HighScoreController
 		for (i = 0; i <= _Scores.Count - 1; i++) {
 			Score s = default(Score);
 
-			s = _Scores.Item(i);
+			s = _Scores[i];
 
 			//for scores 1 - 9 use 01 - 09
 			if (i < 9) {
@@ -149,7 +149,7 @@ static class HighScoreController
 	/// <remarks></remarks>
 	public static void HandleHighScoreInput()
 	{
-		if (SwinGame.MouseClicked(MouseButton.LeftButton) || SwinGame.KeyTyped(KeyCode.VK_ESCAPE) || SwinGame.KeyTyped(KeyCode.VK_RETURN)) {
+		if (SwinGame.MouseClicked(MouseButton.LeftButton) || SwinGame.KeyTyped(KeyCode.vk_ESCAPE) || SwinGame.KeyTyped(KeyCode.vk_RETURN)) {
 			EndCurrentState();
 		}
 	}
@@ -169,7 +169,7 @@ static class HighScoreController
 			LoadScores();
 
 		//is it a high score
-		if (value > _Scores.Item(_Scores.Count - 1).Value) {
+		if (value > _Scores[_Scores.Count - 1].Value) {
 			Score s = new Score();
 			s.Value = value;
 
